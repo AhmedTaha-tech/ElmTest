c1cb21be7ae86829a441699ab9dc5ddc3223e0e5
AhmedTaha-tech <ahmed.taha2027@gmail.com>
2024-01-21 00:17:03 +0200

    Search Books

--- Elm.Books.Api/Controllers/BookController.cs
+++ Elm.Books.Api/Controllers/BookController.cs
@@ -8,14 +8,16 @@ namespace Elm.Books.Api.Controllers
     public class BookController : Controller
     {
         readonly IMediator _mediator;
-
+        /// <summary>
+        /// Serach by Book Title, Book Description,Author, Publish Date (Format yyyy-MM-dd ex:2022-07-17)
+        /// </summary>
         public BookController(IMediator mediator) => _mediator = mediator;
-        [HttpPost(SearchBooksHandler.ROUTE)]
+        [HttpGet(SearchBooksHandler.ROUTE)]
         [ProducesResponseType(typeof(GetBooksResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
 
-        public async Task<ActionResult<GetBooksResponse>> GetBooks([FromBody] BooksModel booksModel)
+        public async Task<ActionResult<GetBooksResponse>> GetBooks([FromQuery] BooksModel booksModel)
                 => Ok(await _mediator.Send(new GetBooksRequest(booksModel)));
     }
 }
--- Elm.Books.Api/Program.cs
+++ Elm.Books.Api/Program.cs
@@ -6,13 +6,22 @@ using System.Net.NetworkInformation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddSingleton<BooksDBContext>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMediatRService();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder => builder
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials()
+                          .SetIsOriginAllowed(origin => true));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
